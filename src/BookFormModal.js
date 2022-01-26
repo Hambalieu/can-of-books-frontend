9bdc895f23eab1a5770b34a7adc85d13e3890bf8
KEVIN LAMARCA <LAMARCA.K@GMAIL.COM>
2022-01-26 00:49:52 -0500

    modal updated

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -40,7 +40,6 @@ class BookFormModal extends React.Component {
                 Submit
               </Button>
             </Form>
-            {/* <img src='https://pbs.twimg.com/profile_images/1455185376876826625/s1AjSxph_400x400.jpg'/> */}
           </Modal.Body>
           <Modal.Footer>
           <Button variant="secondary" onClick={this.props.handleCloseModal}>
